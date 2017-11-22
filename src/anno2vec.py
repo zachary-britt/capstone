0722fb27db1dc7b53b7ae45965849bd926d5372b
zachary-britt <zacharycolebritt@gmail.com>
2017-11-22 14:31:27 -0700

    updating text formatting

--- src/anno2vec.py
+++ src/anno2vec.py
@@ -15,7 +15,8 @@ except ImportError:
 from gensim.models import Word2Vec
 from preshed.counter import PreshCounter
 from spacy.strings import hash_string
-
+import ipdb
+import spacy
 logger = logging.getLogger(__name__)
 
 
@@ -25,8 +26,9 @@ class Corpus:
         self.counts = PreshCounter()
         self.strings = {}
         self.min_freq = min_freq
-
+        self.nlp = spacy.load('en')
     def count_doc(self, words):
+
         # Get counts for this document
         doc_counts = PreshCounter()
         doc_strings = {}
@@ -41,10 +43,70 @@ class Corpus:
             # TODO: Why doesn't inc return this? =/
             corpus_count = self.counts[key]
             # Remember the string when we exceed min count
-            if corpus_count >= self.min_freq and (corpus_count - count) < self.min_freq:
+            if corpus_count >= self.min_freq:  #and (corpus_count - count) < self.min_freq:
                  self.strings[key] = doc_strings[key]
             n += count
         return n
+
+    def add_rare(self, words):
+
+        print("Breaking down rare phrases")
+
+
+        known_word_keys = set(self.strings.keys())
+        new_word_list = []
+        for word in words:
+            word_key = hash_string(word)
+            if not word_key in known_word_keys:
+                word = word[:word.find('|')]
+                new_words = word.replace('_',' ')
+                doc = self.nlp(new_words)
+                new_anno_words = [ '{}|{}'.format(n_word.text,n_word.pos_) for n_word in doc ]
+                new_word_list.extend(new_anno_words)
+
+        print("Broke into {} new words".format(len(new_word_list)))
+
+        ipdb.set_trace()
+
+
+        # Get counts for new_words
+        doc_counts = PreshCounter()
+        doc_strings = {}
+        for word in new_word_list:
+            key = hash_string(word)
+            doc_counts.inc(key, 1)
+            doc_strings[key] = word
+
+        m = 0
+        forlorn_words={}
+        for key, count in doc_counts:
+            self.counts.inc(key, count)
+            # TODO: Why doesn't inc return this? =/
+            corpus_count = self.counts[key]
+            # Remember the string when we exceed min count
+            if corpus_count >= self.min_freq:
+                 self.strings[key] = doc_strings[key]
+            else:
+                forlorn_words[key] = doc_strings[key]
+                m += corpus_count
+
+        print('Could not find a home for {} forlorn words, relabelling'.format(m))
+
+        doc_counts = PreshCounter()
+        doc_strings = {}
+        for word in forlorn_words.items():
+            word = 'RARE'+ word[word.find('|'):]
+            key = hash_string(word)
+            doc_counts.inc(key, 1)
+            doc_strings[key] = word
+
+        for key, count in doc_counts:
+            self.counts.inc(key, count)
+            corpus_count = self.counts[key]
+            #add the word regardless
+            self.strings[key] = doc_strings[key]
+
+
 
     def __iter__(self):
         for text_loc in iter_dir(self.directory):
@@ -95,7 +157,11 @@ def main(in_dir, out_loc, negative=5, n_workers=4, window=5, vec_size=128, min_c
         logger.info("PROGRESS: at batch #%i, processed %i words, keeping %i word types",
                     text_no, total_words, len(corpus.strings))
 
-    
+    #rare processing
+    for text_no, text_loc in enumerate(iter_dir(corpus.directory)):
+        with io.open(text_loc, 'r', encoding='utf8') as file_:
+            text = file_.read()
+        corpus.add_rare(text.split())
 
 
     model.corpus_count = total_sents
--- src/formatter.py
+++ src/formatter.py
@@ -4,6 +4,8 @@ import data_loader as dl
 from multiprocessing import Pool
 import ipdb
 import re
+import string
+
 
 def universal_text_cleaner(text):
 
@@ -32,6 +34,55 @@ def universal_cleaner(df):
     pool.join()
     return df
 
+
+def universal_text_stripper(text):
+    start_afters = [' - ', ' ― ', 'EXCLUSIVE:', 'WASHINGTON', 'Raw video: ']
+    intro = text[:20]
+    for start_after in start_afters:
+        i = intro.find(start_after)
+        if i != -1:
+            text = text[i+len(start_after):]
+            intro = intro[i+len(start_after):]
+
+    #TODO: consistent representative abbreviations
+    # state_abbers = ['AL','AK','AZ','AR','CA',
+    #  'CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD',
+    #  'MA','MI','MN','MS','MO','MT', 'NE', 'NV', 'NH','NJ','NM','NY','NC','ND','OH',
+    #  'OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','WV','WA','VA','WI','WY']
+    #
+    # for ab in state_abbers:
+    #
+    #     text.lower().find_all(ab)
+
+    text = text.replace('\t',' ')
+    text = text.replace(' – ', ', ')
+    text = text.replace(' - ', ', ')
+    s = ' \n                           \n                       \n '
+    i = text.find(s)
+    if i != -1:
+        text = text[i+len(s):]
+
+    strip_chars = ' \n ,'
+    text = text.strip(strip_chars)
+
+    start_afters = ['WASHINGTON,  ', '(AP),  ']
+    intro = text[:20]
+    for start_after in start_afters:
+        i = intro.find(start_after)
+        if i != -1:
+            text = text[i+len(start_after):]
+            intro = intro[i+len(start_after):]
+
+    return text
+
+def universal_stripper(df):
+    from formatter import universal_text_stripper
+    pool = Pool(4)
+    df['content'] = pool.map(universal_text_stripper, df['content'])
+    pool.close()
+    pool.join()
+    return df
+
 def kill_from_(text, keyword, startword):
     i = text.find(keyword)
     if i != -1:
@@ -53,9 +104,13 @@ def fox_clean_text(text):
     '''
 
     sign_ins =  [   ' \n                                      \n                                  \n ',
+                    ' \n                           \n                       \n ',
+                    ' \\n                           \\n                       \\n',
+                    '\n                           \n                       \n'
                     '’Fox & Friends First.’',
-                    '’Special Report.’ \n'
-
+                    '’Special Report.’ \n',
+                    ' AP, File)',
+                    ' has the details. \n'
                 ]
 
     for sign_in in sign_ins:
@@ -66,15 +121,16 @@ def fox_clean_text(text):
     sign_offs = [   ' \n Jake Gibson is a producer',
                     ' \n  Chris Stirewalt',
                     ' \n Chris Stirewalt',
-                    ' \n Follow Fox News’',
+                    ' \n Follow F',
                     ' \n Fox News’',
                     ' \n The Associated Press contributed',
-                    ' Want FOX News',
+                    'Want FOX News',
                     ' \n Brooke Singman is a Politics Reporter ',
                     ' \n Howard Kurtz ',
                     ' \n  News’ Chad Pergram',
                     'Share your color commentary:',
                     '\n Click for more'
+                    '\n Editor’s Note:'
                 ]
 
     for sign_off in sign_offs:
@@ -116,22 +172,25 @@ def hp_clean_text(text):
 
     #ipdb.set_trace()
 
-    aborts = ['(Reuters)']
+    aborts = [  '(Reuters)',
+                'HUFFPOST HILL',
+                'The Morning Email']
 
     for abort in aborts:
         if text.find(abort) != -1:
             return ''
 
-    intro_text = text[:50]
+    #intro_text = text[:50]
 
-    found = re.search(r'—', intro_text)
-    if found:
-        text = text[found.span()[1]:]
+    # found = re.search(r'—', intro_text)
+    # if found:
+    #     text = text[found.span()[1]:]
     # i = intro_text.find(' — ')
     # if i != -1:
     #     text = text[i + len(' — '):]
 
     sign_ins =  [   'Sign up here.) \n '
+                    'WASHINGTON ―'
                 ]
 
     for sign_in in sign_ins:
@@ -180,7 +239,8 @@ def reu_clean_text(text):
      Craft and Peter Cooney'
     '''
 
-    sign_ins =  [  ' (Reuters) -'
+    sign_ins =  [  ' (Reuters) -',
+                    'WASHINGTON'
                 ]
 
     for sign_in in sign_ins:
@@ -204,6 +264,7 @@ def reu_clean_text(text):
         i = text.find(sign_off)
         if i != -1:
             text = text[:i]
+
 
 
     text = text.replace('Reuters', 'this newspaper')
@@ -231,7 +292,7 @@ def find_leak(df, keyword):
 
 def cull_shorts(df):
     lens = np.array(df.content.apply(len))
-    inds = np.argwhere(lens > 100).ravel()
+    inds = np.argwhere(lens > 200).ravel()
     return df.iloc[inds]
 
 def loader_formatter():
@@ -249,6 +310,8 @@ def loader_formatter():
     reu_df = cull_shorts(reu_df)
 
     dfs = [fox_df, hp_df, reu_df]
+
+    dfs = [universal_stripper(df) for df in dfs]
 
     X = np.hstack([ (lambda x: x.content.values)(df) for df in dfs ])
     D = np.hstack([ (lambda x: x.date.values)(df) for df in dfs ])
@@ -273,8 +336,9 @@ def loader_formatter():
     #     np.savez(file_, content=X, date=D, bias=y)
 
     df = pd.DataFrame({'content':contents, 'date':dates, 'bias':biases})
-    df.to_json('/home/zachary/dsi/capstone/data/formatted_arts.json', orient='records',
-            force_ascii=False)
+    #df.save
+    # df.to_json('/home/zachary/dsi/capstone/data/formatted_arts.json', orient='records',
+    #         force_ascii=False)
 
     return df
 
