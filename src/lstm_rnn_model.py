5b06c0f4b7221b336c3395b3ee1f4ff55e2b64b2
zachary-britt <zacharycolebritt@gmail.com>
2017-11-22 22:56:58 -0700

    implemented a non-functional RNN

--- src/lstm_rnn_model.py
+++ src/lstm_rnn_model.py
@@ -1,22 +1,43 @@
 from keras import backend as K
-from keras.layers import CuDNNLSTM, Masking, Dense, Dropout, Activation
+
+''' RNN flavors '''
+
+from keras.layers import RNN, GRU, LSTM, StackedRNNCells, CuDNNLSTM, CuDNNGRU
+
+from keras.layers import Masking, Dense, Dropout, Activation
 from keras.models import Sequential
 from keras.preprocessing.sequence import pad_sequences
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
+from keras import losses
 
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
+
 import numpy as np
 import pandas as pd
 
 import os
 import ipdb
 
+from pprint import pprint
+
+
 def iter_data_files():
     data_dir = '/home/zachary/dsi/capstone/data/df_w_tensors/'
     for file_ in os.listdir(data_dir):
         yield os.path.join(data_dir, file_)
 
+def labeller_bineraizer(y):
+    left = np.where(y=='left', 1, 0).reshape(-1,1)
+    center = np.where(y=='center', 1, 0).reshape(-1,1)
+    right = np.where(y=='right', 1, 0).reshape(-1,1)
+    return np.hstack([left, center, right])
+
+def de_binarize(y):
+    convert = { np.array([1,0,0]):'left',np.array([0,1,0]):'center',np.array([0,0,1]):'right'}
+    ys = np.array([ convert[i] for i in y ])
+    return ys
 
 class Net:
     def __init__(self, seed, verbose=True):
@@ -28,7 +49,7 @@ class Net:
 
         ''' HYPER PARAMETERS '''
         # number of training samples used at a time to update the weights
-        self.batch_size = 30
+        self.batch_size = 64
         # Max number of passes through the entire train dataset (Early stopping
         # prevents unnecessary epochs)
         nb_epoch = 10
@@ -37,7 +58,7 @@ class Net:
         # dense activation function
         dense_activation = 'relu'
         # dense sequence of nodes:
-        dense_sequence = [30, 30]
+        dense_sequence = [60, 60]
 
         #learning rate used by atom
         lr = 0.005
@@ -53,7 +74,7 @@ class Net:
             print(X_test.shape[0], 'test samples')
 
         self.model = Sequential()
-        self.build_lstm()
+        self.build_RNN()
         self.build_layers(dense_sequence, dense_activation)
         self.compile(lr)
 
@@ -63,75 +84,82 @@ class Net:
         self.recorded_score = self.score(X_test, y_test)
 
 
-
     def load_and_configure_data(self):
         # the data, shuffled and split between train and test sets
         #(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
         # load just a couple files for now:
 
+        file_load_count = 10
+
         f_gen = iter_data_files()
-        f1 = next(f_gen)
-        f2 = next(f_gen)
+        file_paths = []
+        for _ in range(file_load_count):
+            file_paths.append(next(f_gen))
 
-        df_1 = pd.read_pickle(f1)
-        df_2 = pd.read_pickle(f2)
+        from multiprocessing import Pool
+        pool = Pool(4)
 
-        df = pd.concat([df_1,df_2])
+        print('Loading DataFrames')
 
-        df_train, df_test = train_test_split(df, test_size = 0.2)
+        dfs = pool.map( pd.read_pickle, file_paths )
+        df = pd.concat(dfs)
 
-        ''' Masking and padding the sequences to be uniform length '''
-        lens = sorted(X_train.tensor.apply( lambda x: x.shape[0]).values)
+        print('DataFrames loaded ')
+
+        df_train, df_test = train_test_split(df, test_size = 0.1)
+
+        ''' padding the sequences to be uniform length '''
+        lens = np.array(sorted(df_train.tensor.apply( lambda x: x.shape[0]).values))
         n_samples = lens.shape[0]
 
-        cutoff_percentile = 0.95
+        #TODO important hyper param? runtime effect?
+        cutoff_percentile = .50
 
         #cut off at sequences longer than cutoff_percentile
         max_len = lens[int(n_samples*cutoff_percentile)]
 
-
-
-
-
-
         X_train = df_train.tensor.values
         X_test = df_test.tensor.values
 
-
+        #TODO important hyper params?
+        X_train = pad_sequences(X_train, max_len , dtype='float32', padding='pre', truncating='pre')
+        X_test = pad_sequences(X_test, max_len , dtype='float32', padding='pre', truncating='pre')
 
 
         y_train = df_train.bias.values
         y_test = df_test.bias.values
 
-        ipdb.set_trace()
 
-        self.vec_dim = X_train[0].shape[1]
+        self.features = X_train[0].shape[1]
+        self.timesteps = max_len
 
         self.nb_classes_ = 3
-
-        def labeller_bineraizer(y):
-            left = np.where(y=='left', 1, 0).reshape(-1,1)
-            center = np.where(y=='center', 1, 0).reshape(-1,1)
-            right = np.where(y=='right', 1, 0).reshape(-1,1)
-            return np.hstack([left, center, right])
 
         y_train = labeller_bineraizer(y_train)
         y_test = labeller_bineraizer(y_test)
 
-        # # convert class vectors to binary class matrices
-        # y_train = np_utils.to_categorical(y_train, self.nb_classes_)
-        # y_test = np_utils.to_categorical(y_test, self.nb_classes_)
-
         return X_train, y_train, X_test, y_test
 
-    def build_lstm(self, **kwrgs):
-        self.model.add(CuDNNLSTM(10,
-            return_sequences=False,
-            stateful=False,
-            kernel_initializer='he_normal',
-            input_shape=(None, self.vec_dim)))
 
+    def build_RNN(self, **kwrgs):
+
+        #self.model.add(Masking(mask_value=0., input_shape=(self.timesteps, self.features)))
+
+
+        self.model.add(CuDNNLSTM(units=30, kernel_initializer='glorot_uniform',
+                                recurrent_initializer='orthogonal', bias_initializer='zeros',
+                                unit_forget_bias=True, kernel_regularizer=None, recurrent_regularizer=None,
+                                bias_regularizer=None, activity_regularizer=None, kernel_constraint=None,
+                                recurrent_constraint=None, bias_constraint=None, return_sequences=False,
+                                return_state=False, stateful=True, input_shape=(self.batch_size, self.timesteps, self.features)))
+
+        # self.model.add(CuDNNLSTM(units=30, kernel_initializer='glorot_uniform',
+        #                         recurrent_initializer='orthogonal', bias_initializer='zeros',
+        #                         unit_forget_bias=True, kernel_regularizer=None, recurrent_regularizer=None,
+        #                         bias_regularizer=None, activity_regularizer=None, kernel_constraint=None,
+        #                         recurrent_constraint=None, bias_constraint=None, return_sequences=False,
+        #                         return_state=False, stateful=False))
 
     def build_layers(self, sequence, activation):
         # self.model.add(Flatten()) # necessary to flatten before going into conventional dense layer
@@ -146,31 +174,25 @@ class Net:
 
 
 
-    def compile(self, lr=0.01):
-        # many optimizers available
-        # see https://keras.io/optimizers/#usage-of-optimizers
-        # suggest you keep loss at 'categorical_crossentropy' for this multiclass problem,
-        # and metrics at 'accuracy'
-        # suggest limiting optimizers to one of these: 'adam', 'adadelta', 'sgd'
-        # how are we going to solve and evaluate it:
-
+    def compile(self, lr=.1):
         adam=Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-        self.model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=["accuracy"] )
+        self.model.compile(loss='categorical_hinge', optimizer=adam, metrics=["accuracy"] )
+
 
     def train(self, X_train, y_train, batch_size, nb_epoch, patience):
-
         # stops training when validation accuracy hasn't improved in "patience" epochs
-        early_stopping=EarlyStopping(monitor='val_acc', min_delta=0,
-            patience=patience, verbose=0, mode='auto')
+        # early_stopping=EarlyStopping(monitor='val_acc', min_delta=0,
+        #     patience=patience, verbose=0, mode='auto')
 
         # during fit process watch train and test error simultaneously
         self.model.fit(X_train, y_train, batch_size=batch_size, epochs=nb_epoch,
-                  verbose=self.verbose, validation_split=0.2, callbacks = [early_stopping])
+                  verbose=self.verbose, validation_split=0.2) #, callbacks = [early_stopping])
 
     def score(self, X_test, y_test):
         score = self.model.evaluate(X_test, y_test, verbose=self.verbose)
         # misses = np.sum self.model.predict
         # print('Test loss:', score[0])
+        pprint(self.model.predict(X_test))
         print('Test accuracy:', score[1]) # this is the one we care about
         return score[1]
 
@@ -182,7 +204,7 @@ if __name__ == "__main__":
     #     net = Net(seed, verbose=False)
     #     scores[i]=net.recorded_score
 
-    net = Net(seed=100, verbose=True)
-    print(net.recorded_score)
+    net = Net(seed=200, verbose=True)
+    #print(net.recorded_score)
 
     # print( "Averaged Score: ", scores.mean())
